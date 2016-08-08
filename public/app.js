d3bcfd2b6f4fd211d03162e86bfb19f1b84821bd
PedroPovoleri <pedro@povoleri.com>
2016-08-08 20:31:05 -0300

    Fixing some loads and add HighCharts for better data visualisation

--- public/app.js
+++ public/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('clavisApp', [
-  'ngRoute',
-  'clavisApp.home'
+var app = angular.module('clavisApp', [
+  'ngRoute'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/home'});
+  $routeProvider.when('/home', {
+    templateUrl: 'home/index.html',
+    controller: 'HomeCtrl'
+  }).otherwise({redirectTo: '/home'});
 }]);
--- public/home/home.js
+++ public/home/home.js
@@ -1,15 +1,6 @@
 'use strict';
 
-angular.module('clavisApp.home', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'home/index.html',
-    controller: 'HomeCtrl'
-  });
-}])
-
-.controller('HomeCtrl', ['$http','$scope',function($http,$scope) {
+app.controller('HomeCtrl', ['$http','$scope',function($http,$scope ) {
 
     var files = $http.get('/api/home').success(function(fileJson){
         return fileJson;
@@ -19,10 +10,11 @@ angular.module('clavisApp.home', ['ngRoute'])
       });
 
     $scope.callCve = function(id) {
-        var cves = $http.get('/api/home/'+id).success(function(fileJson){
+         $http.get('/api/home/'+id).success(function(fileJson){
             return fileJson;
         });
     };
 
 
-}]);
+
+  }]);
